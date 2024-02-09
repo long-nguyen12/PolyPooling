4d5a48a36411111ac5f2a7ad84f36d7b996405c8
Nguyen Long <longnguyenhoang.ict@gmail.com>
2024-02-09 14:21:30 +0700

    update dataset

--- models/datasets/polyp.py
+++ models/datasets/polyp.py
@@ -3,15 +3,11 @@ import numpy as np
 from torch.utils.data import Dataset
 from pathlib import Path
 
-PALETTE = [[0, 0, 0], [255, 255, 255]]
-
 
 class PolypDB(Dataset):
     def __init__(self, root: str, transform=None) -> None:
         super().__init__()
         self.transform = transform
-        self.n_classes = 2
-        self.ignore_label = -1
 
         img_path = Path(root) / "images"
         self.files = list(img_path.glob("*.png"))
@@ -19,14 +15,6 @@ class PolypDB(Dataset):
         if not self.files:
             raise Exception(f"No images found in {img_path}")
         print(f"Found {len(self.files)} images.")
-
-    @staticmethod
-    def convert_to_mask(mask):
-        h, w = mask.shape[:2]
-        seg_mask = np.zeros((h, w, len(PALETTE)))
-        for i, label in enumerate(PALETTE):
-            seg_mask[:, :, i] = np.all(mask == label, axis=-1)
-        return seg_mask
 
     def __len__(self):
         return len(self.files)
@@ -37,16 +25,16 @@ class PolypDB(Dataset):
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        mask = cv2.imread(lbl_path)
-        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
-        mask = self.convert_to_mask(mask)
+        mask = cv2.imread(lbl_path, 0)
+        mask = mask[:, :, np.newaxis]
+        mask = mask.astype("float32") / 255
 
         if self.transform is not None:
             transformed = self.transform(image=image, mask=mask)
 
             image = transformed["image"]
             mask = transformed["mask"]
-            return image.float(), mask.argmax(dim=2).long()
+            return image.float(), mask.permute(2, 0, 1)
 
         else:
             return image.float(), mask.argmax(dim=2).long()
